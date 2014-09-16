2af9b1d3bcfff1a6357477db835133b987af3c16
Bhargav Anand <rjn143@gmail.com>
2014-09-16 19:07:38 -0400

    updated gulpfile and added jshintrc file

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    plugins = require('gulp-load-plugins')();
+
+gulp.task('jshint', function(){
+  return gulp.src(['src/*.js'])
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('annotate', ['jshint'], function(){
+  return gulp.src(['src/*.js'])
+    .pipe(plugins.ngAnnotate({
+      add: true,
+      remove: true,
+      single_quotes: true
+    }))
+    .pipe(plugins.wrapper({
+      header: '(function(){',
+      footer: '}();'
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('src/*.js', ['annotate']);
+});
+
+gulp.task('default', ['watch']);
